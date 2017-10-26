5acc2e329346a6810165268a2fe1121025488b66
Insert <yhhysl@126.com>
2017-10-26 09:22:19 +0800

    atlas
    atlasdemo

---
+++ AtlasDemo/atlas-gradle-plugin/test/src/main/java/com/taobao/android/test.groovy
@@ -0,0 +1,20 @@
+package com.taobao.android;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * Created by wuzhong on 2017/3/15.
+ *
+ * @author wuzhong
+ * @date 2017/03/15
+ */
+public class test implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+
+
+    }
+
+}
