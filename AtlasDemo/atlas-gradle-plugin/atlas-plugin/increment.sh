f82c2e05c199921bd82e70ae31a6d19fbf255acf
Insert <yhhysl@126.com>
2017-10-26 09:04:57 +0800

    Revert "atals"
    This reverts commit 9e80bd3d100c917cceed1ba7d753949aa5d68fde.

--- AtlasDemo/atlas-gradle-plugin/atlas-plugin/increment.sh
+++
@@ -1,4 +0,0 @@
-cd ../../atlas-demo/AtlasDemo/
-./gradlew clean assembleDebug --stacktrace
-./gradlew  assembleDebug --stacktrace
-cd ../../atlas-gradle-plugin/atlas-plugin
--- AtlasDemo/atlas-gradle-plugin/atlas-plugin/test.sh
+++
@@ -1,6 +0,0 @@
-cd ../../atlas-demo/AtlasDemo/
-./gradlew clean assembleDebug --stacktrace
-./gradlew publish
-
-./gradlew clean assembleDebug -DapVersion=1.0.0 --stacktrace
-cd ../../atlas-gradle-plugin/atlas-plugin
